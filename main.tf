ebc522c4c37c16b85e69ffec52874d63e2c4b981
senioris <senioris232@gmail.com>
2024-11-26 11:31:41 +0900

    cross project test

--- main.tf
+++ main.tf
@@ -8,18 +8,18 @@ terraform {
   required_version = "~> 1.9.7"
 
   backend "gcs" {
-    bucket                      = "terrafom-backend-1259452"
-    impersonate_service_account = "terraform@ageless-domain-442510-j3.iam.gserviceaccount.com"
+    bucket                      = "terrafom-backend-idyllic-aspect-442902-h0"
+    impersonate_service_account = "terraform@idyllic-aspect-442902-h0.iam.gserviceaccount.com"
   }  
 }
 
 locals {
-  project_id = "ageless-domain-442510-j3"
+  project_id = "idyllic-aspect-442902-h0"
 }
 
 data "google_service_account_access_token" "default" {
   provider               = google.impersonation
-  target_service_account = "terraform@ageless-domain-442510-j3.iam.gserviceaccount.com"
+  target_service_account = "terraform@idyllic-aspect-442902-h0.iam.gserviceaccount.com"
   scopes                 = ["userinfo-email", "cloud-platform"]
   lifetime               = "3600s"
 }
@@ -34,13 +34,13 @@ provider "google" {
 
 provider "google" {
   project         = local.project_id
-  region          = "asia-northeast2"
+  region          = "asia-northeast1"
   access_token    = data.google_service_account_access_token.default.access_token
   request_timeout = "60s"
 }
 
 resource "google_storage_bucket" "default" {
-  name                        = "terraform-test-2138401367091"
+  name                        = "terraform-cross-project-test-2138401367091"
   location                    = "asia-northeast1"
   force_destroy               = true
   uniform_bucket_level_access = true
